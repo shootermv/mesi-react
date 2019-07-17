b16bc177082313251cc8a86608330b3f0e4c1cdb
Moshe <shootermv@gmail.com>
2019-07-17 14:02:13 +0300

    change login page handlers to use fatarrow syntax (#22)

--- src/LoginPage/LoginPage.jsx
+++ src/LoginPage/LoginPage.jsx
@@ -16,17 +16,14 @@ class LoginPage extends React.Component {
             password: '',
             submitted: false
         };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(e) {
+    handleChange = (e) => {
         const { name, value } = e.target;
         this.setState({ [name]: value });
     }
 
-    handleSubmit(e) {
+    handleSubmit = (e) => {
         e.preventDefault();
 
         this.setState({ submitted: true });
