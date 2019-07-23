ae1fd472ca99750655831ccc52eb25cc6d78a8be
Moshe <shootermv@gmail.com>
2019-07-23 21:40:20 +0300

    added test for "submit" (#26)

--- src/LoginPage/__tests__/LoginPage.spec.jsx
+++ src/LoginPage/__tests__/LoginPage.spec.jsx
@@ -3,6 +3,11 @@ import {shallow} from 'enzyme';
 import {NotConnLoginPage} from '../LoginPage';
   // our mock login function to replace the one provided by mapDispatchToProps
 const mockDispatch = jest.fn();
+
+afterEach(() => {
+  mockDispatch.mockClear();
+});
+
 test('Login page should  have title "Login"', () => {
   // Render a checkbox with label in the document
   const login = shallow(<NotConnLoginPage  dispatch={mockDispatch}/>);
@@ -19,5 +24,22 @@ test('When submitting with empty inputs - should display valiadtion errors"', ()
     {preventDefault() {}}
   )
   expect(login.find('div.help-block').first().text()).toContain('Username is required');
+
+});
+
+test('When submitting with some credentialls - should call "dispatch"', () => {
+  // Render a checkbox with label in the document
+  const login = shallow(<NotConnLoginPage  dispatch={mockDispatch}/>);
+  expect(mockDispatch).toHaveBeenCalledWith({"type": "USERS_LOGOUT"});
+  expect(typeof mockDispatch.mock.calls[0][0]).toBe('object');
+  mockDispatch.mockClear();
+  login.find('input').at(0).simulate('change', { target: { name: 'username', value: 'Changed' } });
+  login.find('input').at(1).simulate('change', { target: { name: 'password', value: 'Changed' } });
+  login.find('form').simulate(
+    'submit', 
+    {preventDefault() {}}
+  )
+  expect(mockDispatch).toHaveBeenCalled();
+  expect(typeof mockDispatch.mock.calls[0][0]).toBe('function');
 
 });
