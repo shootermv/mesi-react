b939cce6b8bbc52817ccf22bed8acec2dec99d88
Moshe <shootermv@gmail.com>
2019-08-05 20:20:54 +0300

    alert action test moved to __tests (#34)
    __

--- src/_actions/__tests__/alert.actions.spec.js
+++ src/_actions/__tests__/alert.actions.spec.js
@@ -1,9 +1,9 @@
-import { alertActions as actions } from './alert.actions';
-import { alertConstants  as types } from '../_constants/alert.constants';
+import { alertActions as actions } from '../alert.actions';
+import { alertConstants  as types } from '../../_constants/alert.constants';
 
 describe('actions', () => {
   it('should create an action success', () => {
-    const message = 'success!'
+    const message = 'success!';
     const expectedAction = {
       type: types.SUCCESS,
       message
