f37fda511735e19aaeb440b44069980ac0898f83
shootermv <shootermv@gmail.com>
2019-08-14 11:59:24 +0300

    special stylesheet added to login

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import styles from './LoginPage.module.css';
 
 import { userActions } from '../_actions';
 
@@ -39,7 +40,7 @@ class LoginPage extends React.Component {
         const { username, password, submitted } = this.state;
         return (
             <div className="col-md-6 col-md-offset-3">
-                <h2>Login</h2>
+                <h2 className={styles.titleColor}>Login</h2>
                 <form name="form" onSubmit={this.handleSubmit}>
                     <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
                         <label htmlFor="username">Username</label>
